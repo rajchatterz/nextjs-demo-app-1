7bdcedce1ad04bc7cf03b4052e2369d9c16486f9
rajchatterz <iamraj.space@gmail.com>
2024-08-16 10:14:29 +0530

    nineth

--- src/app/_component/ResturantSingup.js
+++ src/app/_component/ResturantSingup.js
@@ -13,7 +13,7 @@ const UserSignup = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    let result = await fetch("src/app/api/product", {
+    let result = await fetch("api/product", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
